2f418c3f6b13e8bb1e4ba1ad9729082c87914623
StormAgnew <leeshank96@gmail.com>
2022-08-26 04:36:48 -0500

    Implemented everthing... Stuck on drawing Monster stats. It draws the name and type, but not the stats themselves. Need help figuring it out...

---
+++ Dungeoncrawler/Combat.cs
@@ -0,0 +1,45 @@
+﻿using CharacterInformation;
+using CharacterStats;
+namespace Dungeoncrawler;
+
+public class Combat
+{
+    //This class will not have any fields, properties, or constructors. It is just a method warehouse for combat functionality.
+
+    public static void DoAttack(Stats attacker, Stats defender)
+    {
+        //get a random number from 1 to 100 as our dice roll.
+        Random rand = new Random();
+        int diceroll = rand.Next(1, 101);
+        Thread.Sleep(300); //1000 is one second. This slowes down the game!
+        if (diceroll <= (attacker.Block() - defender.Block()))
+        {
+            //we hit!
+            // calculate damage and save it to a variable.
+            int damageDealt = attacker.CalcDamage();
+            defender.Health -= damageDealt;
+            //write result to screen
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{attacker.Name} hit {defender.Name} for {damageDealt} damage!");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine($"{attacker.Name} missed!");
+        }
+    }
+
+
+
+
+    public static void DoBattle(Stats player, Stats monster)
+    {
+        //player attacks first
+        DoAttack(player, monster);
+        if (monster.Health > 0)
+        {
+            DoAttack(monster, player);
+        }
+
+    }
+}
--- RoomLibrary/Room.cs
+++ RoomLibrary/Room.cs
@@ -28,15 +28,15 @@
         {
             Room r1 = new Room("You enter a very dark and dingy room. Spiders are skittering across the floor, and a drabbed figure is sitting in the corner.\n\n It looks in your direction, but... you don't see its face. A ear-piercing scream shatters the silence, as the figure suddenly appears in front of you.");
 
-            Room r2 = new Room("You enter a room that's rather bright. Almost TOO bright. It's decorated with child-like decorations, from stuffed animals to dollies. In the corner of the room, you see a small creature holding one of the stuffed animals. It appears to be a little girl, hair draping down to her knees. She appears to have been crying, as she looks up at you. Her face is badly bruised, and her lip appears to have been broken open.");
+            Room r2 = new Room("You enter a room that's rather bright. Almost TOO bright.\n It's decorated with child-like decorations, from stuffed animals to dollies. In the corner of the room, you see a small creature holding one of the stuffed animals. It appears to be a little girl, hair draping down to her knees. She appears to have been crying, as she looks up at you. Her face is badly bruised, and her lip appears to have been broken open.");
 
             Room r3 = new Room("Puppy");
 
-            Room r4 = new Room("");
+            Room r4 = new Room("Squirrel");
 
-            Room r5 = new Room("");
+            Room r5 = new Room("Frog");
 
-            Room r6 = new Room("");
+            Room r6 = new Room("Random Animal");
 
             Room[] rooms = { r1, r2, r3, r4, r5, r6};
 
